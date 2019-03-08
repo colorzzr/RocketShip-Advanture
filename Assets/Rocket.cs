2749c401efecaaaae2c718bd428dc8213465b78b
colorzzr <colorzzr@gmail.com>
2019-03-07 23:02:22 -0500

    finish level1

---
+++ Assets/Rocket.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour{
+
+    Rigidbody rb;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void processInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            print("Space Press!");
+            // according to the its own space
+            rb.AddRelativeForce(Vector3.up);
+            // play the sound
+            if(!audioSource.isPlaying) audioSource.Play();
+        }
+        else {
+            audioSource.Stop();
+        }
+
+        rb.freezeRotation = true;
+        //make sure we can rotate in thrust
+        if (Input.GetKey(KeyCode.A)) {
+            print("Left Press!");
+
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            print("Right Press!");
+
+            transform.Rotate(-Vector3.forward);
+        }
+
+        rb.freezeRotation = false;
+    }
+
+    // report while hit something
+    private void OnCollisionEnter(Collision collision) {
+        print("Collide?");
+        switch (collision.gameObject.tag) {
+            case "Platform":
+                print("platform");
+                break;
+            default:
+                print("DIe!");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        processInput();
+        //if(audioSource.isPlaying) 
+    }
+}
